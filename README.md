3027a11cf1b8b3bf2a0f494b6b9a160219db2690
mani1232 <support@worldmandia.cc>
2025-11-27 01:46:57 +0100

    Add jvm flags

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 This is a Kotlin Multiplatform and JVM project targeting Web(with Ktor) and for example Android.
 
 For build just frontend part use `publishKWebUtils`
+
 For build backend(Ktor) with frontend use `:backend:buildFatJar`
--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -39,6 +39,31 @@ ktor {
         jib {
             container {
                 workingDirectory = "/home/container"
+                jvmFlags = listOf(
+                    "-XX:+UnlockExperimentalVMOptions",
+                    "-XX:+UnlockDiagnosticVMOptions",
+                    "-XX:+AlwaysActAsServerClassMachine",
+                    "-XX:+AlwaysPreTouch",
+                    "-XX:+DisableExplicitGC",
+                    "-XX:NmethodSweepActivity=1",
+                    "-XX:ReservedCodeCacheSize=400M",
+                    "-XX:NonNMethodCodeHeapSize=12M",
+                    "-XX:ProfiledCodeHeapSize=194M",
+                    "-XX:NonProfiledCodeHeapSize=194M",
+                    "-XX:-DontCompileHugeMethods",
+                    "-XX:MaxNodeLimit=240000",
+                    "-XX:NodeLimitFudgeFactor=8000",
+                    "-XX:+UseVectorCmov",
+                    "-XX:+PerfDisableSharedMem",
+                    "-XX:+UseFastUnorderedTimeStamps",
+                    "-XX:+UseCriticalJavaThreadPriority",
+                    "-XX:ThreadPriorityPolicy=1",
+                    "-XX:+UseZGC",
+                    "-XX:AllocatePrefetchStyle=1",
+                    "-XX:-ZProactive",
+                    "-Dterminal.jline=false",
+                    "-Dterminal.ansi=true"
+                )
             }
         }
     }
